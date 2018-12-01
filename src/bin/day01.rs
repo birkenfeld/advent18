f799577dc337bd36747b50127ce15458c4843805
Georg Brandl <georg@python.org>
2018-12-01 17:06:12 +0100

    Better naming.

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -5,7 +5,7 @@ fn main() {
     let freq1 = changes.iter().fold(0, |v, n| v + n);
     println!("First round: {}", freq1);
 
-    let (mut accum, mut seen) = (0, HashSet::default());
-    changes.iter().cycle().find(|&n| { accum += n; !seen.insert(accum) });
-    println!("Second round: {:?}", accum);
+    let (mut freq2, mut seen) = (0, HashSet::default());
+    changes.into_iter().cycle().find(|d| { freq2 += d; !seen.insert(freq2) });
+    println!("Second round: {:?}", freq2);
 }
