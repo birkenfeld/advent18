8869702ee13c6ed8f5b7f3e10ee3d18be9e7ba0d
Georg Brandl <georg@python.org>
2018-12-01 07:49:08 +0100

    Day 1.

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,7 +1,11 @@
 use advtools::prelude::*;
 
 fn main() {
-    let input = advtools::input_string();
-    println!("First round: {}", 1);
-    println!("Second round: {}", 2);
+    let changes = advtools::iter_input::<i32>().collect_vec();
+    let freq1 = changes.iter().fold(0, |v, n| v + n);
+    println!("First round: {}", freq1);
+
+    let (mut accum, mut seen) = (0, HashSet::default());
+    changes.iter().cycle().find(|&n| { accum += n; !seen.insert(accum) });
+    println!("Second round: {:?}", accum);
 }
