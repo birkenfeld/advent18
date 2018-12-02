bf28efd5bb2e8e1a79634aabd75233220cfcb41d
Georg Brandl <georg@python.org>
2018-12-02 08:39:05 +0100

    Add some comments.

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,11 +1,15 @@
 use advtools::prelude::*;
 
 fn main() {
+    // Let the helper library parse all lines and collect them.
     let changes = advtools::iter_input::<i32>().collect_vec();
+    // The first part is perfect for a fold(), nothing special going on.
     let freq1 = changes.iter().fold(0, |v, n| v + n);
     println!("First round: {}", freq1);
 
     let (mut freq2, mut seen) = (0, HashSet::default());
+    // Here we use the fact that `HashSet::insert` returns false if the
+    // key was already in the set. cycle() is another nice iterator method.
     changes.into_iter().cycle().find(|d| { freq2 += d; !seen.insert(freq2) });
     println!("Second round: {:?}", freq2);
 }
--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -3,17 +3,29 @@ use advtools::itertools::izip;
 
 fn main() {
     let ids = advtools::iter_input::<String>().collect_vec();
+    // Using fold here lets us keep track of the doubles/triples state
+    // in the iterator without mutable outer variables.
     let (doubles, triples) = ids.iter().fold((0, 0), |(dbls, tpls), id| {
         let mut freqs = HashMap::<_, u32>::default();
+        // Determine frequency of every character in the ID using a hashmap.
         id.chars().for_each(|c| *freqs.entry(c).or_default() += 1);
+        // If we find any of the needed frequency, casting the bool to u32
+        // gives "+ 0" or "+ 1".
         (dbls + freqs.values().any(|&n| n == 2) as u32,
          tpls + freqs.values().any(|&n| n == 3) as u32)
     });
     println!("Checksum: {}", doubles * triples);
 
+    // This part makes heavy use of the goodies in itertools.
+    // tuple_combinations() example: [a, b, c] -> (a, b), (a, c), (b, c)
     for (id, id2) in ids.iter().tuple_combinations() {
+        // izip! lets us zip more than one iterator without nesting pairs.
         let chars = izip!(0.., id.chars(), id2.chars());
+        // collect_tuple() returns `Some` only if there is the exact number of
+        // items.  In this case, we want to find two IDs with exactly *one*
+        // differing position.
         if let Some(((n, ..),)) = chars.filter(|&(_, a, b)| a != b).collect_tuple() {
+            // Take out the differing item and reconstruct a string.
             let others: String = id.chars().take(n).chain(id.chars().skip(n+1)).collect();
             println!("Common ID: {}", others);
             return;
