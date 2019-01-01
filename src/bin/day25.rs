1a1118b05e3a0de0b1dbb58b58547171dd28d3d4
Georg Brandl <georg@python.org>
2019-01-01 15:17:35 +0100

    Day 25.

---
+++ src/bin/day25.rs
@@ -0,0 +1,28 @@
+use advtools::input::iter_input_regex;
+
+const FORMAT: &str = r"(-?\d+),(-?\d+),(-?\d+),(-?\d+)";
+
+fn dist(p1: [i32; 4], p2: [i32; 4]) -> i32 {
+    p1.iter().zip(&p2).map(|(a, b)| (a - b).abs()).sum()
+}
+
+fn main() {
+    let points: Vec<[i32; 4]> = iter_input_regex(FORMAT).collect();
+    let mut constellations: Vec<Vec<_>> = vec![];
+
+    for point in points {
+        // For each point, revise the list of constellations by taking
+        // any that contain the new point, and merging them into one
+        // big new constellation, including the new point.
+        let mut new_constellation = vec![point];
+        for constellation in std::mem::replace(&mut constellations, vec![]) {
+            if constellation.iter().any(|&p| dist(p, point) <= 3) {
+                new_constellation.extend(constellation);
+            } else {
+                constellations.push(constellation);
+            }
+        }
+        constellations.push(new_constellation);
+    }
+    advtools::print("Number of constellations", constellations.len());
+}
